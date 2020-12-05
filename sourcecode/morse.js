9ef231b0b137538793f43f2d8a723d14d02d4b30
Pavan Vineethreddy Thaviti <vineeth.thaviti@github.com>
2020-12-05 19:46:37 +0530

    feat:morse Code converter
    Morse Code Converter

---
+++ sourcecode/morse.js
@@ -0,0 +1,26 @@
+var btnTranslate=document.querySelector("#btn-translate");
+var outputDiv = document.querySelector("#output");
+var textInput= document.querySelector(".textArea");
+var url="http://api.funtranslations.com/translate/morse2english.json";
+btnTranslate.addEventListener("click",clickEvent);
+function clickEvent() {
+    doFetch(textInput.value);
+    
+}
+
+
+function constructUrl(text){
+    return url+"?"+"text="+text;
+}
+function doFetch(text){
+    fetch(constructUrl(text))
+        .then(response => response.json())
+        .then(json => console.log(outputDiv.innerText=json.contents.translated))
+        .catch(errorHandler)
+}
+
+/* error Handling*/
+function errorHandler(error){
+        console.log(('error occurred',error));
+        alert("Please try again later");
+}
